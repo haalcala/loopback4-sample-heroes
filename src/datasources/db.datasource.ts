2524fc2593f6c19945bb4ca02af6ab7d14ddc8a7
Harold Alcala <harold.alcala@vcube.co.jp>
2019-08-05 13:48:40 +0800

    DB Connection settings

--- src/datasources/db.datasource.ts
+++ src/datasources/db.datasource.ts
@@ -1,14 +1,25 @@
-import {inject} from '@loopback/core';
-import {juggler} from '@loopback/repository';
-import * as config from './db.datasource.json';
+import { inject } from "@loopback/core";
+import { juggler } from "@loopback/repository";
+import * as config from "./db.datasource.json";
 
 export class DbDataSource extends juggler.DataSource {
-  static dataSourceName = 'db';
+  static dataSourceName = "db";
 
   constructor(
-    @inject('datasources.config.db', {optional: true})
-    dsConfig: object = config,
+    @inject("datasources.config.db", { optional: true })
+    dsConfig: object = config
   ) {
+    // Override data source config from environment variables
+
+    Object.assign(dsConfig, {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      schema: process.env.DB_SCHEMA
+    });
+
     super(dsConfig);
   }
 }
